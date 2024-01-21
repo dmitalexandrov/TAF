0b14c60cd9327293a9b9f2e3d36984802f586b5a
dmitalexandrov <dmitry.alexandrov@outlook.com>
2024-01-21 16:33:16 +0100

    Add Log

--- Engine/Log.cs
+++ Engine/Log.cs
@@ -1,23 +1,42 @@
 using Xunit.Abstractions;
 
-namespace Bindings
+namespace Engine
 {
     public class Log
     {
-        private ITestOutputHelper output;
+        public ITestOutputHelper output;
         private string logPrefix;
 
-        public Log(ITestOutputHelper o, string l)
+        public Log(ITestOutputHelper o, string l = "=gen")
         {
             output = o;
             logPrefix = l;
         }
 
-        public T Write<T>(string message, T value)
+        public string Write(string message, string value)
         {
             output.WriteLine($"{logPrefix}:: {message} `{value}`");
             Console.WriteLine($"{logPrefix}:: {message} `{value}`");
             return value;
+        }
+
+        public string Write(string message, string value, DateTime dt)
+        {
+            output.WriteLine($"{logPrefix}::{dt.ToString("HH:mm:ss:ff")}:: {message} `{value}`");
+            Console.WriteLine($"{logPrefix}::{dt.ToString("HH:mm:ss:ff")}:: {message} `{value}`");
+            return value;
+        }
+
+        public void Write(string message)
+        {
+            output.WriteLine($"{logPrefix}:: {message}");
+            Console.WriteLine($"{logPrefix}:: {message}");
+        }
+
+        public void Write(string message, DateTime dt)
+        {
+            output.WriteLine($"{logPrefix}::{dt.ToString("HH:mm:ss:ff")}:: {message}");
+            Console.WriteLine($"{logPrefix}::{dt.ToString("HH:mm:ss:ff")}:: {message}");
         }
     }
 }
